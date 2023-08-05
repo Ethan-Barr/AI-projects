1ec59d3161de75ff35ba2125b1e58a6a373d57cf
Ethan Barr <ethanwbarr07@gmail.com>
2023-08-05 18:00:59 +0100

    Finished off Falcon-4B simple terminal usage

--- Falcon-4b/main.py
+++ Falcon-4b/main.py
@@ -7,4 +7,8 @@ def generate(promt, system_promt):
         output = model.generate(promt, max_tokens=4096, n_batch=128)
         return output
 
-response = generate("Hey", "YOu are an AI assist, you are 25 years old and want to help people")
+
+while True:
+    query = input("> ")
+    response = generate(query, "You are an AI assist, you are 25 years old and want to help people") # You can change the system_promt of the the Language model to anything that wou want to make your language model do
+    print(response)
